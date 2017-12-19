6898371fbcfecfe9d9ebd65a06fc327311934aa7
Gio <gbibilashvili@unipay.ge>
2017-12-19 12:30:01 +0400

    Change active tab if

--- src/views/inc/custom/toplist.blade.php
+++ src/views/inc/custom/toplist.blade.php
@@ -1,7 +1,10 @@
 <ul class="nav nav-tabs">
     @foreach($crud->topTabsList as $element)
 
+
+
         @if(!empty($element['list']))
+
             <li role="presentation" class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="{{$element['route']}}" role="button"
                    aria-haspopup="true"
@@ -10,6 +13,8 @@
                 </a>
                 <ul class="dropdown-menu">
                     @foreach($element['list'] as $list)
+
+
 
                         <li role="presentation"
                             class="<?php echo (config('backpack.base.route_prefix') . "/" . $element['route'] === Request::path()) ? "active" : "" ?>">
@@ -22,8 +27,14 @@
                 </ul>
             </li>
         @else
+
+            <?php
+                $route = explode('/', $element['route']);
+                $path = explode('/', Request::path());
+            ?>
+
             <li role="presentation"
-                class="<?php echo (config('backpack.base.route_prefix') . "/" . $element['route'] === Request::path()) ? "active" : "" ?>">
+                class="<?php echo ( $path[count($path)-1] == $route[count($route)-1] ) ? "active" : "" ?>">
                 <a href="{{url(config('backpack.base.route_prefix') . "/" . $element['route'])}}" role="button">
                     {{$element['label']}}
                 </a>
