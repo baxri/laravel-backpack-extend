3f1fb609037397f35f620adef6c0f050a3d79a68
Devi <g.devidze@unipay.ge>
2018-08-01 14:59:05 +0400

    update

--- src/views/inc/custom/route_button.blade.php
+++ src/views/inc/custom/route_button.blade.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: giorgi.devidze
- * Date: 8/1/2018
- * Time: 10:35
- */
+<button type="button" class="btn btn-secondary">Hello</button>
+<script type="text/javascript">
+    function make_request_to_get_total_info( url ){
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {},
+            success: function(data){
+                var panel = $("#totals-panel");
+                panel.html("");
+                $.each( data, function( key, value ) {
+                    panel.append("<p style='color: gray; font-weight: bold; font-size: 15px; padding-top: 8px; float: left; margin-right: 10px;'>" + value.label + ": " + value.value + "</p>");
+                });
+            }
+        });
+    }
+</script>
