a07bb7d0cd85ea6f6a97d951af8537b10fd3f2e2
Devi <g.devidze@unipay.ge>
2018-08-02 10:10:17 +0400

    update custom route button

--- src/views/inc/custom/route_button.blade.php
+++ src/views/inc/custom/route_button.blade.php
@@ -1,16 +1,45 @@
-<button type="button" class="btn btn-secondary">Hello</button>
+<button id="routeButton{{$entry->getKey() . $button->name }}" type="button" class="btn btn-{{ $button->style }} btn-block" data-toggle="modal" data-target="#openModal{{$entry->getKey() . $button->name }}">
+    {{ $button->name }}
+</button>
+
+<div class="modal fade" id="openModal{{$entry->getKey() . $button->name }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{$button->popup_title}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {{$button->popup_description}}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick="make_request_to_route_button('{{$entry->getKey()}}', '{{$button->name}}')" data-dismiss="modal">Yes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script type="text/javascript">
-    function make_request_to_get_total_info( url ){
+    function make_request_to_route_button(key, button){
+
+        var url = '{{$button->route}}';
+        $("#routeButton" + key + button).text("Wait");
+        $("#routeButton" + key + button).attr("disabled", "disabled");
         $.ajax({
             type: "POST",
             url: url,
-            data: {},
+            data: {
+                key: key
+            },
             success: function(data){
-                var panel = $("#totals-panel");
-                panel.html("");
-                $.each( data, function( key, value ) {
-                    panel.append("<p style='color: gray; font-weight: bold; font-size: 15px; padding-top: 8px; float: left; margin-right: 10px;'>" + value.label + ": " + value.value + "</p>");
-                });
+                console.log(key);
+                console.log(button);
+                console.log(data);
+                $("#routeButton" + key + button).text(button);
+                $("#routeButton" + key + button).removeAttr("disabled");
             }
         });
     }
